8a3509646559070766c099b8edf478668a256d74
Ivan Mao <maoyifan714@gmail.com>
2024-05-30 02:36:49 +0800

    Improve document: Element

--- element/line.py
+++ element/line.py
@@ -7,15 +7,51 @@
  @Description : This module contains the Line class used for creating and drawing line in a diagram.
 """
 
-
 class Line:
     """
-    The Signal class represents a signal in a neural network diagram. It has attributes for color, arrow type, width, and line type.
+    A class used to represent a Line in a diagram.
+
+    ...
+
+    Attributes
+    ----------
+    color : str
+        a formatted string to determine the color of the line (default is 'black')
+    arrow_type : str
+        a formatted string to determine the type of the arrow (default is '->')
+    width : str
+        a formatted string to determine the width of the line (default is '1.5pt')
+    line_type : str
+        a formatted string to determine the type of the line (default is 'line')
+    x1, y1, x2, y2 : int
+        integers representing the coordinates of the line (default is (0,0) to (1,1))
+
+    Methods
+    -------
+    generate_id():
+        Generates a unique id for each line by incrementing a class variable 'id_counter'.
+    get_style():
+        Returns a string representing the style of the line.
+    draw():
+        Returns a string that can be used to draw the line in a diagram.
     """
 
     def __init__(self, color='black', arrow_type='->', width='1.5pt', line_type='line', x1=0, y1=0, x2=1, y2=1):
         """
-        Initialize a Signal object with the given color, arrow type, width, and line type. Defaults are provided for all parameters.
+        Constructs all the necessary attributes for the Line object.
+
+        Parameters
+        ----------
+            color : str
+                a formatted string to determine the color of the line (default is 'black')
+            arrow_type : str
+                a formatted string to determine the type of the arrow (default is '->')
+            width : str
+                a formatted string to determine the width of the line (default is '1.5pt')
+            line_type : str
+                a formatted string to determine the type of the line (default is 'line')
+            x1, y1, x2, y2 : int
+                integers representing the coordinates of the line (default is (0,0) to (1,1))
         """
         self.color = color
         self.arrow_type = arrow_type
@@ -30,17 +66,35 @@ class Line:
 
     @classmethod
     def generate_id(cls):
+        """
+        Generates a unique id for each line by incrementing a class variable 'id_counter'.
+
+        Returns
+        -------
+        int
+            a unique id for the line
+        """
         cls.id_counter += 1
         return cls.id_counter
 
     def get_style(self):
         """
-        Returns a string representing the style of the signal in LaTeX format.
+        Returns a string representing the style of the line.
+
+        Returns
+        -------
+        str
+            a string representing the style of the line
         """
         return self.__class__.__name__ + f"""/.style={{draw={self.color}, {self.arrow_type}, {self.line_type} width={self.width}}},"""
 
     def draw(self):
         """
-        Public method that calls the private __draw method with the given arguments.
+        Returns a string that can be used to draw the line in a diagram.
+
+        Returns
+        -------
+        str
+            a string that can be used to draw the line in a diagram
         """
         return f'\n\draw[{self.__class__.__name__}] ({self.x1},{self.y1}) -- ({self.x2},{self.y2});'
